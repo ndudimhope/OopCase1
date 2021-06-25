d02da8e58207fd84e60edd4215896b41c2b9ae80
Hope Ndudim <hope.ndudim@thebulb.africa>
2021-06-25 23:46:15 +0100

    Add project files.

---
+++ OopCase1.Objects/Telephone.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopCase1.OopCase1.Objects
+{
+    class Telephone
+    {
+        private readonly string _manufacturer;
+        private readonly string _model;
+
+        public Telephone()
+        {
+
+        }
+
+        public void MakeCall()
+        {
+
+        }
+
+        public void RecieveCall()
+        {
+
+        }
+    }
+}
